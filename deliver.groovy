90bd4fd01c1738cf9d3b019c4fc799105c598cc1
Matt Rendina <mrendina@stsci.edu>
2018-03-12 15:32:03 -0400

    Deposit environment specification files in loation of choice.

--- deliver.groovy
+++ deliver.groovy
@@ -1,10 +1,10 @@
 @Library('utils') _
 // Parameters made available from Jenkins job configuration:
 //   delivery_name
-//   delivery_iteration
 //   aux_packages
 //   conda_installer_version
 //   conda_version
+//   output_dir
 
 def gen_specfiles(label) {
     
@@ -15,7 +15,6 @@ def gen_specfiles(label) {
         deleteDir()
 
         unstash "script"
-        sh "pwd; ls -al"
 
         env.PYTHONPATH = ""
         // Make the log a bit more deterministic
@@ -27,7 +26,7 @@ def gen_specfiles(label) {
         conda.install()
 
         PATH = "${WORKDIR}/miniconda/bin:${PATH}"
-        def cpkgs = "conda=${CONDA_VERSION}"
+        def cpkgs = "conda=${conda_version}"
         def pkg_list = aux_packages.replaceAll('\n', ' ')
 
         withEnv(["HOME=${HOME}", "PATH=${PATH}"]) {
@@ -53,10 +52,9 @@ node('master') {
                        daysToKeepStr: '',
                        numToKeepStr: '4')), pipelineTriggers([])])
     stage('create specfiles') {
-        sh "pwd; ls -al"
+        deleteDir()
         sh "cp -r ${WORKSPACE}@script/*.sh ."
         stash name: "script", includes: "*.sh"
-        sh "ls -al"
         parallel(
             Linux: { gen_specfiles('RHEL-6') },
             MacOS: { gen_specfiles('OSX-10.11') }
@@ -67,6 +65,11 @@ node('master') {
         // Retrieve the spec files from the nodes where they were created.
         unstash "spec-stash-RHEL-6"
         unstash "spec-stash-OSX-10.11"
-        archive "hstdp*txt"
+        hostname = sh(script: "hostname", returnStdout: true).tokenize(".")[0]
+        withCredentials([usernamePassword(credentialsId: '322ad15d-2f5b-4d06-87fa-b45a88596f30',
+            usernameVariable: 'USERNAME',
+            passwordVariable: 'PASSWORD')]) {
+                sh "rsync -avzr hstdp*.txt ${USERNAME}@${hostname}:${output_dir}"
+           }
     }
 }
